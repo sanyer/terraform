60bed5ddb2e3533f447475d60189eafdd7747d2c
Radek Simko <radek.simko@gmail.com>
2020-02-13 05:08:15 +0000

    lang/langserver: initial copy from f-langserver

--- command/show_config.go
+++ command/show_config.go
@@ -26,69 +26,16 @@ func (c *ShowConfigCommand) Run(args []string) int {
 	var jsonOutput bool
 	var atPos int
 	cmdFlags.BoolVar(&jsonOutput, "json", false, "produce JSON output")
-	cmdFlags.IntVar(&atPos, "at-pos", -1, "at position (in byte offset from the beginning of a file)")
+	cmdFlags.IntVar(&atPos, "at-pos", -1, "at position (in byte offset format)")
 	cmdFlags.Usage = func() { c.Ui.Error(c.Help()) }
 	if err := cmdFlags.Parse(args); err != nil {
 		c.Ui.Error(fmt.Sprintf("Error parsing command-line flags: %s\n", err.Error()))
 		return 1
 	}
 
-	args = cmdFlags.Args()
-	if len(args) > 1 {
-		c.Ui.Error("TODO")
-		cmdFlags.Usage()
-		return 1
-	}
-
-	// Check for user-supplied plugin path
-	if c.pluginPath, err = c.loadPluginPath(); err != nil {
-		c.Ui.Error(fmt.Sprintf("Error loading plugin path: %s", err))
-		return 1
-	}
-
-	var diags tfdiags.Diagnostics
-
-	// Load the backend
-	b, backendDiags := c.Backend(nil)
-	diags = diags.Append(backendDiags)
-	if backendDiags.HasErrors() {
-		c.showDiagnostics(diags)
-		return 1
-	}
-
-	// We require a local backend
-	local, ok := b.(backend.Local)
-	if !ok {
-		c.showDiagnostics(diags) // in case of any warnings in here
-		c.Ui.Error(ErrUnsupportedLocalOp)
-		return 1
-	}
-
-	// the command expects the config dir to always be the cwd
-	cwd, err := os.Getwd()
+	configPath, err := ModulePath(cmdFlags.Args())
 	if err != nil {
-		c.Ui.Error(fmt.Sprintf("Error getting cwd: %s", err))
-		return 1
-	}
-
-	// TODO: Determine if a config file was passed to the command
-
-	// Build the operation
-	opReq := c.Operation(b)
-	opReq.ConfigDir = cwd
-	opReq.ConfigLoader, err = c.initConfigLoader()
-	opReq.AllowUnsetVariables = true
-	if err != nil {
-		diags = diags.Append(err)
-		c.showDiagnostics(diags)
-		return 1
-	}
-
-	// Get the context
-	ctx, _, ctxDiags := local.Context(opReq)
-	diags = diags.Append(ctxDiags)
-	if ctxDiags.HasErrors() {
-		c.showDiagnostics(diags)
+		c.Ui.Error(err.Error())
 		return 1
 	}
 
@@ -120,6 +67,8 @@ Usage: terraform show-config [options] [path]
   from the current workdir will be shown.
 
 Options:
+
+  -at-pos At position (in byte offset format)
 
   -json   If specified, output the configuration in
           a machine-readable form.
